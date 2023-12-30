f4b8f34c0b75fc926275ada61e12adc238b8b578
Guhan <118154546+guhan1308@users.noreply.github.com>
2023-12-30 14:02:33 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('2');
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
